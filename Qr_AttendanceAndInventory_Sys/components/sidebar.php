e05ffea52a7caf6e034f48cc66149eee34957f13
JasperjohnPaulSCruz <123532247+rfidparkingbitch@users.noreply.github.com>
2024-06-16 21:21:14 +0800

    homepage and students designed

--- Qr_AttendanceAndInventory_Sys/components/sidebar.php
+++ Qr_AttendanceAndInventory_Sys/components/sidebar.php
@@ -1,76 +1,63 @@
 
-<aside id="default-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-   <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
-      <ul class="space-y-2 font-medium">
-         <li>
-            <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-               <svg class="w-5 h-5 text-gray-500 transition duration-75" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
-                  <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
-                  <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
-               </svg>
-               <span class="ms-3">Dashboard</span>
+<aside id="default-sidebar" class="fixed pl-5 py-3  h-full " >
+   <div class="w-[70px] flex justify-center items-center max-h-screen-xl py-5 bg-white h-full rounded-lg my-auto shadow-md shadow-green-200">
+      <ul class="w-full flex flex-col items-center font-medium py-3 rounded-lg">
+
+        <div class="flex w-full justify-center items-center">
+          <li class="my-5">
+            <a href="home" class="menu ">
+            <svg class="text-[30px]" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><rect width="7" height="7" x="3" y="3" rx="1"/><rect width="7" height="7" x="14" y="3" rx="1"/><rect width="7" height="7" x="14" y="14" rx="1"/><rect width="7" height="7" x="3" y="14" rx="1"/></g></svg>
             </a>
-         </li>
-         <li>
-            <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-               <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
-                  <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z"/>
-               </svg>
-               <span class="flex-1 ms-3 whitespace-nowrap">Kanban</span>
-               <span class="inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-700 dark:text-gray-300">Pro</span>
+          </li>
+          <div id="home" class=" hidden absolute right-0 h-[40px] border-[6px] border-transparent border-r-green-500 content-none rounded-lg"></div>
+        </div>
+
+        <div class="flex w-full justify-center items-center">
+          <li class="my-5">
+            <a href="students" class="menu ">
+              <svg class="text-[30px]" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M18 21a8 8 0 0 0-16 0"/><circle cx="10" cy="8" r="5"/><path d="M22 20c0-3.37-2-6.5-4-8a5 5 0 0 0-.45-8.3"/></g></svg>
             </a>
-         </li>
-         <li>
-            <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-               <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                  <path d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z"/>
-               </svg>
-               <span class="flex-1 ms-3 whitespace-nowrap">Inbox</span>
-               <span class="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">3</span>
+          </li>
+          <div id="students" class=" hidden absolute right-0 h-[40px] border-[6px] border-transparent border-r-green-500 content-none rounded-lg"></div>
+        </div>
+
+        <div class="flex w-full justify-center items-center">
+          <li class="my-5">
+            <a href="inventory" class="menu ">
+              <svg class="text-[30px]" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M2.97 12.92A2 2 0 0 0 2 14.63v3.24a2 2 0 0 0 .97 1.71l3 1.8a2 2 0 0 0 2.06 0L12 19v-5.5l-5-3zM7 16.5l-4.74-2.85M7 16.5l5-3m-5 3v5.17m5-8.17V19l3.97 2.38a2 2 0 0 0 2.06 0l3-1.8a2 2 0 0 0 .97-1.71v-3.24a2 2 0 0 0-.97-1.71L17 10.5zm5 3l-5-3m5 3l4.74-2.85M17 16.5v5.17"/><path d="M7.97 4.42A2 2 0 0 0 7 6.13v4.37l5 3l5-3V6.13a2 2 0 0 0-.97-1.71l-3-1.8a2 2 0 0 0-2.06 0zM12 8L7.26 5.15M12 8l4.74-2.85M12 13.5V8"/></g></svg>          
             </a>
-         </li>
-         <li>
-            <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-               <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
-                  <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"/>
-               </svg>
-               <span class="flex-1 ms-3 whitespace-nowrap">Users</span>
+          </li>
+          <div id="inventory" class=" hidden absolute right-0 h-[40px] border-[6px] border-transparent border-r-green-500 content-none rounded-lg"></div>
+        </div>
+
+        <div class="flex w-full justify-center items-center">
+          <li class="my-5">
+            <a href="about" class="menu ">
+              <svg class="text-[30px]" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><circle cx="12" cy="12" r="10"/><path d="M12 8v4m0 4h.01"/></g></svg>
             </a>
-         </li>
-         <li>
-            <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-               <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
-                  <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"/>
-               </svg>
-               <span class="flex-1 ms-3 whitespace-nowrap">Students</span>
-            </a>
-         </li>
-         <li>
-            <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-               <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
-                  <path d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z"/>
-               </svg>
-               <span class="flex-1 ms-3 whitespace-nowrap">Products</span>
-            </a>
-         </li>
-         <li>
-            <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-               <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"/>
-               </svg>
-               <span class="flex-1 ms-3 whitespace-nowrap">Sign In</span>
-            </a>
-         </li>
-         <li>
-            <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-               <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                  <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.96 2.96 0 0 0 .13 5H5Z"/>
-                  <path d="M6.737 11.061a2.961 2.961 0 0 1 .81-1.515l6.117-6.116A4.839 4.839 0 0 1 16 2.141V2a1.97 1.97 0 0 0-1.933-2H7v5a2 2 0 0 1-2 2H0v11a1.969 1.969 0 0 0 1.933 2h12.134A1.97 1.97 0 0 0 16 18v-3.093l-1.546 1.546c-.413.413-.94.695-1.513.81l-3.4.679a2.947 2.947 0 0 1-1.85-.227 2.96 2.96 0 0 1-1.635-3.257l.681-3.397Z"/>
-                  <path d="M8.961 16a.93.93 0 0 0 .189-.019l3.4-.679a.961.961 0 0 0 .49-.263l6.118-6.117a2.884 2.884 0 0 0-4.079-4.078l-6.117 6.117a.96.96 0 0 0-.263.491l-.679 3.4A.961.961 0 0 0 8.961 16Zm7.477-9.8a.958.958 0 0 1 .68-.281.961.961 0 0 1 .682 1.644l-.315.315-1.36-1.36.313-.318Zm-5.911 5.911 4.236-4.236 1.359 1.359-4.236 4.237-1.7.339.341-1.699Z"/>
-               </svg>
-               <span class="flex-1 ms-3 whitespace-nowrap">Sign Up</span>
-            </a>
-         </li>
+          </li>
+          <div id="about" class=" hidden absolute right-0 h-[40px] border-[6px] border-transparent border-r-green-500 content-none rounded-lg"></div>
+        </div>
+
       </ul>
    </div>
 </aside>
+
+<script>
+
+const path = window.location.pathname;
+const pathName = path.replace('/Qr_Attendance/Qr_AttendanceAndInventory_Sys/', '');
+
+const links = document.querySelectorAll("a.menu");
+
+links.forEach(link => {
+
+    if (pathName === link.getAttribute('href')) {
+        link.classList.add("text-green-700");
+        document.getElementById(pathName).classList.remove("hidden");;
+    } else {
+        link.classList.add("text-green-200");
+    }
+});
+
+</script>
--- Qr_AttendanceAndInventory_Sys/components/topbar.php
+++ Qr_AttendanceAndInventory_Sys/components/topbar.php
@@ -1,43 +1,80 @@
+<script src="https://unpkg.com/flowbite@1.5.1/dist/flowbite.js"></script>
+
+<?php include "sidebar.php";?>
+
+<nav class="absolute pt-3 w-full border-gray-200 ">
+  <div class="max-w-screen-xl px-7  bg-white rounded-lg flex flex-wrap items-center justify-between mx-auto shadow-md shadow-green-200">
+    <div class=" w-auto flex justify-center items-center" id="navbar-multi-level">
+      
+      <p id="pathName" class="font-extrabold tracking-wide text-gray-900 text-[18px]"></p>
+    </div>
+
+    <div class="flex items-center font-poppins">
+      
+        <div class="flex mr-5 gap-5">
+            <!-- dropdown section -->
+            <select id="states" class="px-5 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 w-full py-2">
+                <option selected>Year&Section</option>
+                <option value="1a">1A</option>
+                <option value="2a">2A</option>
+                <option value="2b">2B</option>
+                <option value="3a">3A</option>
+                <option value="4a">4A</option>
+            </select>
+             <!-- dropdown group -->
+            <select id="states" class="px-5 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 w-full py-2">
+                <option selected>Group</option>
+                <option class="hover:bg-green-500" value="1">1</option>
+                <option class="hover:bg-green-500" value="2">2</option>
+            </select>
+
+        </div>
 
 
-<nav class="absolute w-full bg-white border-gray-200 shadow-lg">
-  <div class="max-w-screen-xl flex flex-wrap items-center justify-end mx-auto">
-    <div class=" w-auto block" id="navbar-multi-level">
-      <ul class="flex flex-col font-medium p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
-        <li>
-          <a href="home" class="menu block py-2 px-3 text-gray-900 rounded">Home</a>
-        </li>
-        <li>
-          <a href="students" class="menu block py-2 px-3 text-gray-900 rounded">Students</a>
-        </li>
-        <li>
-          <a href="inventory" class="menu block py-2 px-3 text-gray-900 rounded">Inventory</a>
-        </li>
-        <li>
-          <a href="#" class="menu block py-2 px-3 text-gray-900 rounded">About</a>
-        </li>
-      </ul>
+      
+      <div class="border border-gray-300 content-none h-[30px] mr-[20px]"></div>
+      <div class="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+
+        <button type="button" class="px-1 py-2 flex justify-between align-center items-center text-sm rounded-full md:me-0" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+          <img class="w-12 h-12 rounded-full" src="https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250" alt="user photo">
+          <div class="px-3">
+            <p class="font-bold tracking-wide text-[16px] text-gray-900 text-start">Juan Dela Cruz Jr.</p>
+            <p class="text-gray-500 tracking-wide text-start font-medium ">Faculty</p>
+          </div>
+        </button>
+
+        <!-- Dropdown menu -->
+        <div class="z-50 hidden my-4 text-base list-none divide-y divide-gray-100 rounded-lg shadow-md bg-emerald-950" id="user-dropdown">
+          <ul class="py-2" aria-labelledby="user-menu-button">
+            <li>
+              <a href="#" class="block px-10 py-2 text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Profile</a>
+            </li>
+            <li>
+              <a href="#" class="block px-10 py-2 text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Change Password</a>
+            </li>
+            <li>
+              <a href="#" class="block px-10 py-2 text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
 </nav>
 
+
 <script>
 
-const path = window.location.pathname;
-const pathName = path.replace('/Qr_Attendance/Qr_AttendanceAndInventory_Sys/', '');
+const route = window.location.pathname;
+const  routeName = path.replace('/Qr_Attendance/Qr_AttendanceAndInventory_Sys/', '');
 
-const links = document.querySelectorAll("a.menu");
-links.forEach(link => {
-  console.log(link.getAttribute('href'));
+function upperInitial(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
 
-    if (link.getAttribute('href') === pathName) {
+let titleName = upperInitial(routeName);
+var titlePage = document.getElementById('pathName');
 
-        link.classList.add("text-white", "bg-lime-700", "hover:bg-lime-500");
-    } else {
-        link.classList.add("text-gray-900", "hover:bg-gray-100" , "hover:text-lime-700");
-    }
-    console.log('.');
-});
-
+titlePage.innerText = titleName;
 
 </script>
